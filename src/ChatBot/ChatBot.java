c7a4c140b835034903e3942a03a1ab62e90470b9
Kovalenko Matvey <csgogoldwin@gmail.com>
2023-12-30 23:56:06 +0200

    1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //1st stage
+        System.out.println("Hello! My name is MyBot.");
+        System.out.println("I was created in 2023.");
