175ae5e75c11c8fd614746ee545a1770628de284
Kovalenko Matvey <csgogoldwin@gmail.com>
2023-12-30 23:42:44 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("javaeducation");
     }
 
+=======
+        System.out.println("education");
+        //comment on second string of the func
+        System.out.println("DICT");
+    }
+}
+>>>>>>> c81af1b (added comment in Test.java at the feature_branch)
